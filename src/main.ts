7b4a6d7b747bb34cbae83d763e529ec772ad9cc2
huifeng-kooboo <gisdoing@gmail.com>
2023-04-05 18:15:14 +0800

    add push

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// 1. 引入UI组件
+import { Button } from 'vant'
+// 2. 引入组件样式
+import 'vant/lib/index.css'
+
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
