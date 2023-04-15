731406b69beced8ec83077db111166b3107522d0
huifeng-kooboo <gisdoing@gmail.com>
2023-04-15 19:56:20 +0800

    fix: 界面样式调整

--- src/views/ToningInterface.vue
+++ src/views/ToningInterface.vue
@@ -7,9 +7,6 @@ import { ref } from 'vue';
 
 <template> 
   <div class="toningInteface">
-    <van-row>
-        <label style="color: black;">FBX文件显示</label>
-    </van-row>
     <van-row justify="center">
         <van-button round type="primary" @click="showCurrentModel">加载模型文件</van-button>
     </van-row>
@@ -46,16 +43,12 @@ import { ref } from 'vue';
 
     <van-row justify="center" class="saverow">
         <van-col>
-            <van-button round type="primary">保存设置</van-button>
+            <van-button round type="primary" style="width: 180px;">保存设置</van-button>
         </van-col>
     </van-row>
     <van-row>
         <van-divider />
     </van-row>
-    <!-- <van-grid clickable :column-num="2">
-        <van-grid-item style="background-color: aqua;" icon="home-o" text="首页" to="/" />
-        <van-grid-item style="background-color: aqua;" icon="manager" text="个人中心" to="/personalCenter" />
-    </van-grid> -->
     <ChoosePage />
   </div>
 </template>
