f101dea67baee16fffc0665863ea1c76a717b8c5
huifeng-kooboo <gisdoing@gmail.com>
2023-04-16 02:13:42 +0800

    fix: 界面样式调整

--- src/components/ChoosePage.vue
+++ src/components/ChoosePage.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <van-grid clickable :column-num="3">
+        <van-grid clickable :column-num="3" >
             <van-grid-item style="background-color: aqua;" icon-color="#2ea6e2" icon="home-o"  to="/" />
-            <van-grid-item style="background-color: aqua;" icon-color="#2ea6e2" icon="plus"  to="/" />
+            <!-- <van-grid-item style="background-color: aqua;" icon-color="#2ea6e2" icon="plus"  to="/" /> -->
+            <van-button type="primary" icon="plus" class="btnPlus" round style="width: 80px; margin-top: 10px;"></van-button>
             <van-grid-item style="background-color: aqua;" icon-color="#2ea6e2" icon="manager" to="/personalCenter" />
         </van-grid>
     </div>
