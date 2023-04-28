225c378031aedb434102f9cb79ce2530f0a32313
DESKTOP-R154IOJ\duhuifeng <huifeng813@xmov.ai>
2023-04-28 16:31:16 +0800

    add

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,7 @@ module.exports = {
             //1.必须添加http前缀，没添加我运行不了
             //2.localhost、127.0.0.1、公网地址三者都可以使用
             //3.结尾加不加/都ok
-            target: 'http://127.0.0.1:8000',
+            target: 'http://192.168.93.193:8000',
             // 允许跨域
             changeOrigin: true,
             ws: true,
