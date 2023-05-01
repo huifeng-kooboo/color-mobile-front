b27b34a2293925d20cb897c62a72795911b5540c
huifeng-kooboo <gisdoing@gmail.com>
2023-05-01 14:53:26 +0800

    delete unused components

--- src/api/service/upload.ts
+++
@@ -1,9 +0,0 @@
-import axios from "../axios"
-
-
-export function uploadFileApi(params) {
-    axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-    return axios.post("/api/v1/file_manager/file_info/", params);
-}
-
-export { uploadFileApi };
