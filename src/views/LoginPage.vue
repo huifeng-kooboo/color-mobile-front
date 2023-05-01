51126fc4c55680fbaef2a83c7fe062f8ef8955c7
huifeng-kooboo <gisdoing@gmail.com>
2023-05-01 23:42:06 +0800

    样式调整

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -11,19 +11,19 @@ import { ref } from 'vue';
     </van-row>
     <van-row class="loginTitle" justify="center">
         <van-col>
-            <h2 style="color: black; margin-top: 20px; font-size: 14px;">登录界面</h2>
+            <h2 style="color: black; margin-top: 20px; margin-bottom: 20px; font-size: 24px; font-weight:bold;">登录界面</h2>
         </van-col>
     </van-row>
     <van-row class="loginInput" justify="center">
         <van-col>
             <van-form @submit="onSubmit">
                 <van-cell-group inset>
-                    <van-field v-model="username" name="用户名" label="用户名" placeholder="用户名"
+                    <van-field v-model="username" name="用户名" label="用户名" placeholder="请输入用户名"
                         :rules="[{ required: true, message: '请填写用户名' }]" />
-                    <van-field v-model="password" type="password" name="密码" label="密码" placeholder="密码"
+                    <van-field v-model="password" type="password" name="密码" label="密码" placeholder="请输入密码"
                         :rules="[{ required: true, message: '请填写密码' }]" />
                 </van-cell-group>
-                <div style="margin: 16px;">
+                <div style="margin: 16px; margin-top:20px;">
                     <van-button round block type="primary" native-type="submit">
                         登录
                     </van-button>
